3190ecd1de87c329918cb36555c826b2ec1c5289
Tafshi Uthshow Hoque <69767127+Draxgter1001@users.noreply.github.com>
2026-07-23 14:45:47 +0100

    feat: implement Employee class with attributes, constructor, and comparison logic

--- Week 1/Day 4/Employee Payroll System/Employee.java
+++ Week 1/Day 4/Employee Payroll System/Employee.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+public class Employee implements Comparable<Employee> {
 
-public class Employee {
+    private final int id;
+    private final String name;
+    private final String department;
+    private final double salary;
+
+    public Employee(int id, String name, String department, double salary) {
+        if(name == null || department == null || salary <+ 0){ throw new IllegalArgumentException();}
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public double getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee Details - " + "ID: " + getId() + ", Name: " + getName() + ", Department: " + getDepartment() +
+                ", Salary: " + getSalary();
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        return Double.compare(this.salary, o.salary);
+    }
+
 }
